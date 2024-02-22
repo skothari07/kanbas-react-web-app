f5599ec3938323264b1f72c5fa3542e20f0dee69
skothari07 <skothari855@gmail.com>
2024-02-22 03:40:24 -0500

    Base completed - A3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,8 @@ import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
+import Grades from "./Grades";
 
 function Courses() {
     const { courseId } = useParams();
@@ -28,8 +30,8 @@ function Courses() {
                         <Route path="Modules" element={<Modules/>} />
                         <Route path="Piazza" element={<h1>Piazza</h1>} />
                         <Route path="Assignments" element={<Assignments />} />
-                        <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
-                        <Route path="Grades" element={<h1>Grades</h1>} />
+                        <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>} />
+                        <Route path="Grades" element={<Grades />} />
                     </Routes>
                 </div>
             </div>
