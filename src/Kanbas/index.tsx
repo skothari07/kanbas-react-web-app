84344a471b7871474ce791903d5dc80d04a24a48
skothari07 <skothari855@gmail.com>
2024-04-04 12:21:38 -0400

    A6 troubleshooting

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -9,6 +9,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import store from "./store";
 import { Provider } from "react-redux";
+import Account from "./Account";
 
 function Kanbas() {
 
@@ -75,8 +76,8 @@ function Kanbas() {
                     </div>
                     <div style={{ flexGrow: 1 }}>
                         <Routes>
+                            <Route path="/Account/*" element={<Account />} />
                             <Route path="/" element={<Navigate to="Dashboard" />} />
-                            <Route path="Account" element={<h1>Account</h1>} />
                             <Route path="Dashboard" element={<Dashboard
                                                                 courses={courses}
                                                                 course={course}
