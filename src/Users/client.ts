6bfd119f982ddb6221f97d2dcb3a285956d8694d
skothari07 <skothari855@gmail.com>
2024-04-22 00:18:37 -0400

    debug

--- src/Users/client.ts
+++ src/Users/client.ts
@@ -12,6 +12,7 @@ const request = axios.create({
 });
 
 export const signin = async (credentials: User) => {
+    console.log(`${USERS_API}/signin`);
     const response = await request.post(`${USERS_API}/signin`, credentials);
     return response.data;
 };
